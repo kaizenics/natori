2e7bfdcb0e877e7496aa9eb9b935377a5b99b608
Niko <nicosejohnsoriano@gmail.com>
2024-02-27 00:12:01 +0800

    fix: navbar background color

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,7 +3,7 @@ import { ModeToggle } from "~/components/ui/toggle";
 export const Navbar = () => {
   return (
     <>
-      <nav className="flex justify-between px-12 py-5 sticky top-0 dark:bg-zinc-950 bg-opacity-70 backdrop-filter backdrop-blur-lg z-10">
+      <nav className="flex justify-between px-12 py-5 sticky top-0 bg-transparent bg-opacity-90 backdrop-filter backdrop-blur-lg z-10">
         <div className="flex-row justify-between items-center pt-1">
           <div className="cursor-pointer">
             <div className="w-[30px] h-[2px] my-[6px] bg-black dark:bg-white"></div>
